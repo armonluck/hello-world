eb5f2755a44012bf39e76db98a115478e73494d0
Armon Luckenbach <armonluck@gmail.com>
2022-10-06 19:36:23 -0400

    Moved JS functions to an external sheet and linked to HTML

---
+++ app.js
@@ -0,0 +1,11 @@
+
+let userName = prompt("What is your name?");
+	document.write("Greetings, " + userName + " welcome to my page!")
+	console.log('User entered: ' + userName);
+
+let userAnswer = prompt('Do you like to code?'); 
+    if(userAnswer == 'yes'){
+     document.write("You like to code? So do I!");
+    } else {
+     document.write('It is not for everybody')
+    } 
